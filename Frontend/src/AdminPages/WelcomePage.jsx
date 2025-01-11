fed8ded4788800b4eaf001865268992d904e5e9d
Emad Ali Khan <emadalikhan5@gmail.com>
2025-01-12 04:55:18 +0500

    welcomePage changes

--- Frontend/src/AdminPages/WelcomePage.jsx
+++ Frontend/src/AdminPages/WelcomePage.jsx
@@ -9,8 +9,45 @@ const WelcomePage = () => {
     navigate("/adminPortal/items");
   }, 5000);
   return (
-    <Box
-      sx={{
+    // <Box
+    //   sx={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     alignItems: "center",
+    //     justifyContent: "center",
+    //     height: "100vh",
+    //     textAlign: "center",
+    //     backgroundColor: "rgb(295, 150, 0)",
+    //     color: "black",
+    //     padding: 3,
+    //   }}
+    // >
+    //   <Typography
+    //     variant="h2"
+    //     component="h1"
+    //     gutterBottom
+    //     className="jacques-francois-shadow-regular"
+    //   >
+    //     Welcome to the Society Portal
+    //   </Typography>
+    //   <Typography
+    //     variant="h5"
+    //     component="p"
+    //     className="jacques-francois-shadow-regular"
+    //   >
+    //     Manage your restaurant efficiently and effectively.
+    //   </Typography>
+    //   <Typography
+    //     variant="h5"
+    //     component="p"
+    //     className="jacques-francois-shadow-regular"
+    //   >
+    //     You will redirecting to the dashboard in a few seconds...
+    //   </Typography>
+    // </Box>
+    <div
+      className="jacques-francois-shadow-regular"
+      style={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
@@ -22,16 +59,37 @@ const WelcomePage = () => {
         padding: 3,
       }}
     >
-      <Typography variant="h2" component="h1" gutterBottom>
-        Welcome to the Admin Portal
-      </Typography>
-      <Typography variant="h5" component="p">
-        Manage your restaurant efficiently and effectively.
-      </Typography>
-      <Typography variant="h5" component="p">
+      <h1
+        style={{
+          fontSize: "60px",
+        }}
+      >
+        {" "}
+        Welcome to the{" "}
+        <b className="text-light">
+          <u>Society Portal</u>
+        </b>
+      </h1>
+      <h1
+        style={{
+          fontSize: "30px",
+          paddingTop: "20px",
+        }}
+      >
+        {" "}
+        Manage your Restaurant <u className="text-light">
+          efficiently
+        </u> and <u className="text-light">effectively</u>.
+      </h1>
+      <h1
+        style={{
+          fontSize: "20px",
+        }}
+      >
+        {" "}
         You will redirecting to the dashboard in a few seconds...
-      </Typography>
-    </Box>
+      </h1>
+    </div>
   );
 };
 
